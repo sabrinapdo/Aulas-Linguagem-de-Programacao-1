8e91a887fdf12c69b7eaca68cc4639e6ffe893bd
sabrinapdo <sabrina.wv.1111@gmail.com>
2026-03-09 18:06:44 -0300

    Update desvio_multiplo.py

--- Aula 04 - Estruturas de Decisao/python_logic/desvio_multiplo.py
+++ Aula 04 - Estruturas de Decisao/python_logic/desvio_multiplo.py
@@ -1,8 +1,8 @@
-quantidade = int(input("Digite a quantidade em estoque"))
+quantidade = int(input("Digite a quantidade em estoque: "))
 
 if quantidade < 1:
     print("Status: Em falta.")
-elif quantidade < 5: #elif é a forma abreviada de else e if
-    print("Status: Alerta de estoque baixo")
+elif quantidade < 5: #elif é a forma abreviada de 'else e if'
+    print("Status: Alerta de estoque baixo.")
 else:
     print("Status: Estoque OK.")
