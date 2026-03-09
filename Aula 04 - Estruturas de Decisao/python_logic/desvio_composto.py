43570d2a0bcb275d413395ea1e50f90b65b1314f
sabrinapdo <sabrina.wv.1111@gmail.com>
2026-03-09 18:02:12 -0300

    Update desvio_composto.py

--- Aula 04 - Estruturas de Decisao/python_logic/desvio_composto.py
+++ Aula 04 - Estruturas de Decisao/python_logic/desvio_composto.py
@@ -1,8 +1,6 @@
-# Solicitando a quantidade ao usuário
-
 quantidade = int(input("Digite a quantidade no estoque: "))
-# usuando a estrutura 'if'
+
 if quantidade < 5:
-    print("Aviso: o estoque desse produto está baixo!")
+    print("AVISO: o estoque desse produto está baixo!")
 else:
     print("Estoque OK.")
